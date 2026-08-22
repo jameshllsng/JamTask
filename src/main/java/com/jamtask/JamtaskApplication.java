91f0d88a194a955b357264fa15f8596632dcdc53
jameshllsng <lmsandrincardoso@gmail.com>
2026-08-22 03:59:37 +0000

    chore: initialize spring boot project

---
+++ src/main/java/com/jamtask/JamtaskApplication.java
@@ -0,0 +1,13 @@
+package com.jamtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamtask/JamtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
