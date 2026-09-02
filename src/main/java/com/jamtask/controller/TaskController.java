22af534c0759f537892fedf85cab892a6676cbc3
jameshllsng <lmsandrincardoso@gmail.com>
2026-09-02 03:04:31 +0000

    feat: implement task creation

--- src/main/java/com/jamtask/controller/TaskController.java
+++ src/main/java/com/jamtask/controller/TaskController.java
@@ -1,12 +1,34 @@
 package com.jamtask.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jamtask.entity.Task;
+import com.jamtask.entity.TaskStatus;
+import com.jamtask.service.TaskService;
 
 @Controller
 public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping("/tasks/new")
     public String showCreateForm() {
         return "tasks/form";
+    }
+
+    @PostMapping("/tasks")
+    public String createTask(@RequestParam String title, @RequestParam String description, @RequestParam TaskStatus status, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dueDate) {
+        Task task = new Task(title, description, status, dueDate);
+        taskService.createTask(task);
+        return "redirect:/tasks/new";
     }
 }
