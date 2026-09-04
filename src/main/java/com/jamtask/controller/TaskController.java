5d80ad7456e47b4e8d865d847dde378d2aa1edc5
jameshllsng <lmsandrincardoso@gmail.com>
2026-09-04 01:47:12 +0000

    feat: implement task listing

--- src/main/java/com/jamtask/controller/TaskController.java
+++ src/main/java/com/jamtask/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +30,12 @@ public class TaskController {
     public String createTask(@RequestParam String title, @RequestParam String description, @RequestParam TaskStatus status, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dueDate) {
         Task task = new Task(title, description, status, dueDate);
         taskService.createTask(task);
-        return "redirect:/tasks/new";
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/tasks")
+    public String listTasks(Model model) {
+        model.addAttribute("tasks", taskService.findAllTasks());
+        return "tasks/list";
     }
 }
