afbd528e12fb6418271faf331d9f01a3293dae95
jameshllsng <lmsandrincardoso@gmail.com>
2026-09-18 22:50:12 -0300

    feat: add task deletion service method

--- src/main/java/com/jamtask/service/TaskService.java
+++ src/main/java/com/jamtask/service/TaskService.java
@@ -38,4 +38,9 @@ public class TaskService {
 
         return taskRepository.save(task);
     }
+
+    public void deleteTask(Long id) {
+        Task task = findTaskById(id);
+        taskRepository.delete(task);
+    }
 }
